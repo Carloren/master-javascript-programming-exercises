a274bc6c90d43bc368761b3262051f2cd6054634
Carlos Lorenzo <carloren.1996@gmail.com>
2025-04-21 10:46:21 +0000

    Pending changes exported from your codespace

--- exercises/003-isOldEnoughToVote/app.js
+++ exercises/003-isOldEnoughToVote/app.js
@@ -1,4 +1,10 @@
 function isOldEnoughToVote(age) {
   // your code here
-  
+  if(age>=18){
+    return true;
+  }else{
+      return false;
+  }
 }
+let oldEnough = isOldEnoughToVote(21);
+console.log(oldEnough);
