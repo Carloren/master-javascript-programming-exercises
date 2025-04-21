fb3942edbbf3185a377564a68220c641685a8a68
Carlos Lorenzo <carloren.1996@gmail.com>
2025-04-21 10:58:55 +0000

    Ejercicio completado

--- exercises/014-isLessThan/app.js
+++ exercises/014-isLessThan/app.js
@@ -1,8 +1,9 @@
 function isLessThan(num1, num2) {
   // your code here
-  if (num2 < num1){
+  if (num2 < num1) {
     return true;
-  } else{
+  } else {
     return false;
   }
 }
+console.log(isLessThan(10, 8));
