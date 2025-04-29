be5609b89144e29bd7ec641a831fa72f83e95737
Carlos Lorenzo <carloren.1996@gmail.com>
2025-04-29 09:24:45 +0000

    Ejercicio completado

--- exercises/049-computeAverageOfNumbers/app.js
+++ exercises/049-computeAverageOfNumbers/app.js
@@ -1,1 +1,16 @@
 // Write your function here
+function computeAverageOfNumbers(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i]
+    }
+    if (arr.length != 0) {
+        let avg = sum / arr.length;
+        return avg;
+    } else {
+        return 0;
+    }
+}
+let input = [1, 2, 3, 4, 5];
+let output = computeAverageOfNumbers(input);
+console.log(output); // --> 3
